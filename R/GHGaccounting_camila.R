055a21b0a8c56aeca7df12e0d8d306e5fc11a72c
camilavargasp <cvargas@bren.ucsb.edu>
2024-05-10 08:44:11 -0700

    Adding notes to script on how to read the GHG data to make instructions clear.

--- R/GHGaccounting_camila.R
+++ R/GHGaccounting_camila.R
@@ -1,9 +1,14 @@
-## Script with general file path that make radng in the GHG data more straight forward (R project is set up accordingly)
-## In this case all file paths to files ar relatcive to directories within the Rproj
-## Note that the dambay file is ignored by git given the size of the file. You can access this file by downloading from the SharePoint folder: Infrastructue Group > Data Sets and Products > GHG > dem_bay_delta_10m_20201207.zip
+
+## Script with general file path that make reading in the GHG data more straight forward (R project is set up accordingly)
+
+## In this case all file paths to files ar relative to directories within the Rproj or directly from a url
+
+## Note that the dambay file is ignored by git given the size of the file. You can access this file by downloading from the SharePoint folder: Infrastructure Group > Data Sets and Products > GHG > dem_bay_delta_10m_20201207.zip
+
+## Safe the dem_bay files inside thedata-raw folder and use the code below to read in the data.
 
 
-dembay <- rast("data-raw/dem_bay_delta_10m_20201207.tif")
+dembayr <- rast("data-raw/dem_bay_delta_10m_20201207.tif")
 
 dembayp <- project(dembay, "EPSG:3310")
 
