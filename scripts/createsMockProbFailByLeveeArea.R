5c850ca35fe97d4bb5810aa4f394273d523fb43b
camilavargasp <cvargas@bren.ucsb.edu>
2024-06-21 15:05:14 -0700

    creating a temporary mock data with porb of failur and levee areas. This data is to be able to build app while figuring out how to get the real data.

---
+++ scripts/createsMockProbFailByLeveeArea.R
@@ -0,0 +1,46 @@
+# This scripts creates the format for probability levee failure data withing each levee ares based on tthe RAND prob of Failure data and the fixed levee island
+
+
+# Load Packages ----
+library(tidyverse)
+library(janitor)
+library(sf)
+
+
+# Read data ----
+
+prob_fail <- read_csv(file.path("data-clean", "randProbFailure", "probFailure.csv")) %>% 
+  clean_names()
+
+leveed_areas <- st_read(file.path("data-clean", "shapefiles", "fixedLevees", "leveedAreas.shp")) %>% 
+  clean_names() %>% 
+  mutate(lma = str_to_lower(lma))
+
+
+# Filter prob_fail to baseline scenario ----
+prob_fail_baseline <- prob_fail %>% 
+  filter(slr_condition == "baseline",
+         lf_curve == 4,
+         discount_rate == 0.00) %>% 
+  select(island_id, island_or_tract, year, hydrologic_failure, seismic_failure, levee_failure) %>% 
+  mutate(island_or_tract = str_to_lower(island_or_tract))
+
+# Assigning a prob of failure to a each levee ares ----
+
+# NOTE: THIS IS NOT A LOGIT STEP! THIS IS JUST TO HAVE AN EXAMPLE DATA TO WORK ON THE SHINY APP. THERE IS NOT DIRECT MATCH OF LEVEE AREA TO ISLANDS PRESERNTED IN THEN PROB OF FAILURE. 
+
+prob_fail_area <- leveed_areas %>% 
+  left_join(prob_fail_baseline, join_by(lma == island_or_tract)) %>% 
+  filter(!is.na(levee_failure))
+
+
+plot(prob_fail_area["levee_failure"])
+
+
+# save mock data to use in example app ----
+write_sf(prob_fail_area, file.path("data-clean", "mock-data", "mock_prob_fail_levee_area.shp"))
+
+
+
+
+
