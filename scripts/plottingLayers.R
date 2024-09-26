8327d145b3749db9c9826815bd5fd23729ba73eb
camilavargasp <cvargas@bren.ucsb.edu>
2024-09-26 22:12:33 +0000

    finalizing map with all layers (adding structure value by bins and levee polygons)

--- scripts/plottingLayers.R
+++ scripts/plottingLayers.R
@@ -7,23 +7,28 @@ library(sf)
 library(htmltools)
 
 ## Read data
-
 prob_fail <- st_read("data-clean/shapefiles/probFailure/prob_fail_levee_area.shp")
 st_crs(prob_fail)
 prob_fail_w84 <- prob_fail %>% 
   st_transform('+proj=longlat +datum=WGS84')
 
 structure_value <- st_read("data-clean/shapefiles/nationalStructureInventory/nsi_2024_total_value_levee_area.shp")%>%
-  st_transform('+proj=longlat +datum=WGS84')
+  st_transform('+proj=longlat +datum=WGS84') %>% 
+  rename(structure_value_area = strct__,
+         total_value_trill = ttl_vl_)
 
 managed_wetlands <- st_read("data-clean/shapefiles/managedWetlands/managedWetlands.shp") %>% 
   st_transform('+proj=longlat +datum=WGS84')
 
 croplands <- st_read("data-clean/shapefiles/deltaCropland/croplandsByType.shp") %>% 
-  st_transform('+proj=longlat +datum=WGS84')
+  st_transform('+proj=longlat +datum=WGS84') %>% 
+  rename(total_crop_area = ttl_cr_)
 
 soc_vul <- st_read("data-clean/shapefiles/leveeLSDayPopWeightSVI/leveeLSDayPopWeightSVI.shp") %>% 
   select(LMA, area, leveeAr, RPL_THEMES) %>% 
+  st_transform('+proj=longlat +datum=WGS84')
+
+levee_areas <- st_read("data-clean/shapefiles/fixedLevees/leveedAreas.shp") %>% 
   st_transform('+proj=longlat +datum=WGS84')
 
 ## Even though all layers where initially in CA Albers, Leaflet was giving me a warning about the need to re project into '+proj=longlat +datum=WGS84'. Now all data layers have been converted to the projection Leaflet likes.
@@ -47,18 +52,27 @@ managed_wetlands_pal <- colorFactor(
 # na.color = "gray")
 
 ## Croplands
-unique(croplands$Clss_Nm)
+unique(croplands$type)
 
 crops_pal <- colorFactor(
   palette = rainbow,
-  domain  = croplands_complete$type)
+  domain  = croplands$type)
 
 ## Social Vulnerability
-
 soc_vul_pal <- colorNumeric(
   palette = "viridis", 
   domain = soc_vul$RPL_THEMES)
 # na.color = "gray")
+
+## structure value (bins)
+range(structure_value$total_value_trill, na.rm = T)
+
+## structure color pallet
+bins <- c(0, 1, 10, 100, 1000, 20000, 30000, 40000, 70000)
+structure_pal <- colorBin("YlOrRd", 
+                          domain = structure_value$total_value_trill,
+                          bins = bins)
+
 
 
 ## Plot ----
@@ -70,6 +84,13 @@ leaflet() %>%
   addProviderTiles("Esri.WorldImagery", 
                    group = "Imagery") %>%
   ## Add all polygon layers
+  addPolygons(data = levee_areas,
+              group = "Levee Areas",
+              label = ~htmlEscape(
+                paste("Name:", LMA)),
+              color="black", # polygon border color
+              fill = NA,
+              weight=1.2) %>% 
   addPolygons(data = soc_vul,
               group = "Social Vulnerability",
               fillColor = ~soc_vul_pal(RPL_THEMES),
@@ -79,13 +100,13 @@ leaflet() %>%
               stroke = TRUE,
               fillOpacity = 0.6, 
               color="black", # polygon border color
-              weight=0.8, ) %>% # polygon border weight
+              weight=0.8 ) %>% # polygon border weight
   addPolygons(data = prob_fail_w84,
               group = "Probability of Failure",
               fillColor = ~prob_fail_pal(lev_flr),
               # label = soc_vul_labs,
               stroke = TRUE,
-              fillOpacity = 0.6, 
+              fillOpacity = 0.6,
               color="black", # polygon border color
               weight=0.8, ) %>% # polygon border weight
   addPolygons(data = managed_wetlands,
@@ -93,17 +114,25 @@ leaflet() %>%
               fillColor = ~managed_wetlands_pal(PM_LndT),
               # label = soc_vul_labs,
               stroke = TRUE,
-              fillOpacity = 0.6, 
+              fillOpacity = 0.6,
               color="black", # polygon border color
               weight=0.8, ) %>% # polygon border weight
-  addPolygons(data = croplands_complete,
+  addPolygons(data = croplands,
               group = "Croplands",
               fillColor = ~crops_pal(type),
               # label = soc_vul_labs,
               stroke = TRUE,
-              fillOpacity = 0.6, 
+              fillOpacity = 0.6,
               color="black", # polygon border color
               weight=0.8, ) %>% # polygon border weight
+  addPolygons(data = structure_value,
+              group = "Structure",
+              fillColor = ~structure_pal(total_value_trill),
+              # label = soc_vul_labs,
+              stroke = TRUE,
+              fillOpacity = 0.6,
+              color = "black", # remove polygon border
+              weight=0.8, ) %>%
   ## Legend layers
   addLegend(group = "Social Vulnerability",
             pal = soc_vul_pal,
@@ -122,89 +151,33 @@ leaflet() %>%
             values = managed_wetlands$PM_LndT,
             opacity = 0.6,
             title = "Habitat Type",
-            position = "bottomleft") %>% 
+            position = "bottomleft") %>%
+  addLegend(group = "Croplands",
+            pal = crops_pal,
+            values = croplands$type,
+            opacity = 0.6,
+            title = "Cropland Group",
+            position = "bottomleft") %>%
+  addLegend(group = "Structure",
+            pal = structure_pal,
+            values = structure_value$total_value_trill,
+            opacity = 0.6,
+            title = "Structure Value (Trillions)",
+            position = "bottomleft") %>%
   addLayersControl(
     baseGroups = c("Grey background", "Imagery"),
-    overlayGroups = c("Social Vulnerability",
+    overlayGroups = c("Levee Areas",
+                      "Social Vulnerability",
                       "Probability of Failure",
-                      "Managed Wetlands"),
+                      "Habitat Type",
+                      "Croplands",
+                      "Structure"),
     options = layersControlOptions(collapsed = FALSE)) %>%
   # hide these groups by default
   hideGroup(c("Social Vulnerability",
               "Probability of Failure",
               "Habitat Type",
-              "Croplands")) %>% 
+              "Croplands",
+              "Structure")) %>%
   setView(lat=38.2, lng=-121.7, zoom=9)
-
-
-
-
-
-
-## ------------
-
-cropland_pal <- colorFactor(
-  palette = rainbow,
-  domain  = croplands$type)
-
-## Plot for structure by bins
-
-leaflet() %>%
-  ## add tiles
-  addProviderTiles(providers$CartoDB.Positron, 
-                   group = "Grey background") %>%
-  ## Add all polygon layers
-  addPolygons(data = croplands,
-              group = "Croplands",
-              fillColor = ~cropland_pal(type),
-              # label = soc_vul_labs,
-              stroke = TRUE,
-              fillOpacity = 0.6, 
-              color = NA, # remove polygon border
-              weight=0.8, ) %>% # polygon border weight
-  addLegend(group = "Cropland",
-            pal = cropland_pal,
-            values = croplands$type,
-            opacity = 0.6,
-            title = "Cropland Type",
-            position = "bottomleft") %>%
-  setView(lat=38.2, lng=-121.7, zoom=9)
-
-
-## Structure value by bins ----
-range(structure_value$t__2024, na.rm = T)
-
-## structure color pallet
-bins <- c(0, 10^12, 10^13, 10^14, 10^15, 2*10^16, 3*10^16, 4*10^16, 7*10^16)
-structure_pal <- colorBin("YlOrRd", 
-                domain = structure_value$t__2024, 
-                bins = bins)
-
-## structue map ----
-leaflet() %>%
-  ## add tiles
-  addProviderTiles(providers$CartoDB.Positron, 
-                   group = "Grey background") %>%
-  ## Add all polygon layers
-  addPolygons(data = structure_value,
-              group = "Structure",
-              fillColor = ~structure_pal(t__2024),
-              # label = soc_vul_labs,
-              stroke = TRUE,
-              fillOpacity = 0.6, 
-              color = "black", # remove polygon border
-              weight=0.8, ) %>% # polygon border weight
-  addLegend(group = "Structure",
-            pal = structure_pal,
-            values = structure_value$t__2024,
-            opacity = 0.6,
-            title = "Structure Value",
-            position = "bottomleft") %>%
-  setView(lat=38.2, lng=-121.7, zoom=9)
-
-## NEED TO FIGURE OUT HOW TO GET THE VALLUES OF THE LEGEND INTO SCEINTIFIC NOTATION
-
-value_under_10 <- structure_value %>% 
-  filter(t__2024 < 10000000000000000)
-
 
